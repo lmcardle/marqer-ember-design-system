98c9da814244543ac38fe4db8576577b115f2c35
Liam McArdle <mcardle.liam@gmail.com>
2018-03-24 15:09:10 -0700

    add bulma css to project

---
+++ blueprints/marqer-components/index.js
@@ -0,0 +1,39 @@
+/* eslint-env node */
+'use strict';
+
+const path = require('path');
+const fs   = require('fs');
+
+module.exports = {
+  description: '',
+
+  normalizeEntityName() {},
+
+  afterInstall() {
+    let stylePath = path.join('app', 'styles');
+    let filePath      = path.join(stylePath, 'app.scss');
+    let folderNames   = ['utilities', 'base', 'elements', 'components', 'grid', 'layout'];
+
+    let importStatement = folderNames.map(function(folder) {
+      return `@import "marqer-components/${folder}/_all";`;
+    }).join('\n');
+
+    if (!fs.existsSync(stylePath)) fs.mkdirSync(stylePath);
+
+    if (fs.existsSync(filePath)) {
+      this.ui.writeLine(`Added import statement to ${filePath}`);
+      return this.insertIntoFile(filePath, importStatement, {});
+    } else {
+      fs.writeFileSync(filePath, importStatement);
+      this.ui.writeLine(`Created ${filePath}`);
+    }
+
+    return this.addAddonsToProject({
+      packages: [{name: 'ember-cli-sass'}]
+    }).then(() => {
+      return this.addPackagesToProject([
+        {name: 'bulma', target: '^0.6.2'}
+      ]);
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 'use strict';
 
+const Funnel    = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'marqer-components'
+  name: 'marqer-components',
+
+  treeForStyles: function treeForStyles(tree) {
+    let styleTrees = [];
+
+    if (this.app.project.findAddonByName('ember-cli-sass')) {
+      styleTrees.push(new Funnel('node_modules/bulma/sass', {
+        destDir: 'marqer-components'
+      }));
+    }
+
+    if (tree) {
+      styleTrees.push(tree);
+    }
+
+    return mergeTrees(styleTrees, {overwrite: true});
+  },
+
+  included: function() {
+    this._super.included.apply(this, arguments);
+  }
 };
